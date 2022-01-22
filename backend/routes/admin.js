5fc61e397659b1685d37e42709ee5d58919960e5
JayWoo2000 <joewoo2000>
2022-01-22 12:23:13 -0500

    create data beta

---
+++ backend/routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+
+router.get('/', adminController.getIndex);
+
+router.get('/add-user', adminController.getAddUser);
+router.get('/add-item', adminController.getAddItem);
+
+router.post('/add-user', adminController.postUser);
+router.post('/add-item', adminController.postItem);
+
+
+module.exports = router;
