c27ac7582ef1a2849c7eca168b51a8555ef679a9
Avery Schaefer <54673851+AverySchaefer@users.noreply.github.com>
2022-01-23 05:22:43 -0500

    add login method to userRouter

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { User } from '../models/user';
+import { itemRouter } from './item';
 
 const userRouter = Router();
 
@@ -52,6 +53,50 @@ itemRouter.get('/:userID', async (req, res) => {
 	}
 
     res.status(200).send({ user });
+});
+
+//Log in to an existing user's account
+itemRouter.post('/login', async (req, res) => {
+	let { email, password } = req.body;
+
+	if (!password || !email) {
+		res.status(400).send({ msg: 'Password and email must be provided.' });
+		return;
+	}
+
+    let user;
+    let userID;
+	try {
+		user = await User.findOne({ email }).exec();
+        userID = user._id;
+
+		// Reject if user does not exist
+		if (!user) {
+			res.status(400).send({ msg: 'No user with email exists.' });
+			return;
+		}
+	} catch (e) {
+		// Handle internal server errors
+		console.error(e);
+		res.status(500).send({ msg: 'An error occurred when fetching user.' });
+		return;
+	}
+
+    try {
+		if (password != user.password) {
+			res.status(400).send({ msg: 'Invalid password.' });
+			return;
+		}
+	} catch (e) {
+		console.error(e);
+		res.status(500).send({ msg: 'An error occurred when comparing password.' });
+		return;
+	}
+
+    res.status(200).send({
+		userID,
+		msg: 'Successfully logged in.',
+	});
 });
 
 export { userRouter };
