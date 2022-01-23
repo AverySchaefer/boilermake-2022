d481eda24555cc4c6e4462854f274376266f2199
Kyochul-Jang <jang128@purdue.edu>
2022-01-23 06:41:08 -0500

    Accept Feedback2.

--- college-exchange/src/components/Home/Items.js
+++ college-exchange/src/components/Home/Items.js
@@ -4,7 +4,7 @@ export const Items = [
     imageURL:
       "https://www.seriouseats.com/thmb/Gm6J49wltvTCH2dXt039zkvGZLg=/880x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__2018__04__20180309-french-fries-vicky-wasik-15-5a9844742c2446c7a7be9fbd41b6e27d.jpg",
     productName: "French Fries",
-    price: "12$",
+    price: "$12",
     sellerName: "Joseph",
     ratings: "4",
   },
@@ -12,7 +12,7 @@ export const Items = [
     cname: "item",
     imageURL: "https://cdna.4imprint.com/prod/rend/300/238716/15_15.jpg",
     productName: "Eraser",
-    price: "10$",
+    price: "$10",
     sellerName: "Kyochul",
     ratings: "5",
   },
@@ -21,7 +21,7 @@ export const Items = [
     imageURL:
       "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MV702?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1602803373000",
     productName: "Earphone",
-    price: "100$",
+    price: "$100",
     sellerName: "Jay",
     ratings: "4.5",
   },
--- college-exchange/src/components/UserProfile/HistoryItems.js
+++ college-exchange/src/components/UserProfile/HistoryItems.js
@@ -4,39 +4,37 @@ export const HistoryItems = [
       "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c10fcd90c433455e8f79ad0800cb4469_9366/NMD_R1_Primeblue_Shoes_Black_GZ9257_01_standard.jpg",
     alter: "Alt",
     product: "Shoes",
-    price: "2000$",
+    price: "$2000",
     cName: "history-items",
   },
   {
     image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c10fcd90c433455e8f79ad0800cb4469_9366/NMD_R1_Primeblue_Shoes_Black_GZ9257_01_standard.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1280px-A_small_cup_of_coffee.JPG",
     alter: "Alt",
-    product: "Shoes",
-    price: "2000$",
+    product: "Coffee",
+    price: "$3",
+    cName: "history-items",
+  },
+  {
+    image: "https://static.toiimg.com/thumb/59666824.cms?width=1200&height=900",
+    alter: "Alt",
+    product: "Jelly",
+    price: "$8",
+    cName: "history-items",
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71MQeIS7FAL._SL1500_.jpg",
+    alter: "Alt",
+    product: "Doritos",
+    price: "$8",
     cName: "history-items",
   },
   {
     image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c10fcd90c433455e8f79ad0800cb4469_9366/NMD_R1_Primeblue_Shoes_Black_GZ9257_01_standard.jpg",
+      "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6441/6441105_sd.jpg;maxHeight=640;maxWidth=550",
     alter: "Alt",
-    product: "Shoes",
-    price: "2000$",
-    cName: "history-items",
-  },
-  {
-    image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c10fcd90c433455e8f79ad0800cb4469_9366/NMD_R1_Primeblue_Shoes_Black_GZ9257_01_standard.jpg",
-    alter: "Alt",
-    product: "Shoes",
-    price: "2000$",
-    cName: "history-items",
-  },
-  {
-    image:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/c10fcd90c433455e8f79ad0800cb4469_9366/NMD_R1_Primeblue_Shoes_Black_GZ9257_01_standard.jpg",
-    alter: "Alt",
-    product: "Shoes",
-    price: "2000$",
+    product: "Phone",
+    price: "$1800",
     cName: "history-items",
   },
 ];
